b64cfa893b4469f97ea6bdf4f85a8402c30bac84
Laura Brown <laurabrown1113@gmail.com>
2013-03-26 14:27:49 -0400

    working

--- config/schedule.rb
+++ config/schedule.rb
@@ -21,9 +21,11 @@
 
 set :cron_log, "~/log/cron_log.log"
 set :whenever_command, "bundle exec whenever"
+set :environment, "development"
 
 
-every 1.day, :at => '11:49 pm' do
+
+every 1.day, :at => '11:59 pm' do
   runner "PredictionMailer.send_todays_emails"
 end
 
