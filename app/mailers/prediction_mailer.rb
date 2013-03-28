525cf702fc2afaa50fdfdb89266986f7dbbef618
Laura Brown <laurabrown1113@gmail.com>
2013-03-28 09:52:05 -0400

    image in email working

--- app/mailers/prediction_mailer.rb
+++ app/mailers/prediction_mailer.rb
@@ -4,7 +4,6 @@ class PredictionMailer < ActionMailer::Base
   def prediction_email(prediction)
     @prediction = prediction
     mail(:to => prediction.email.email, :subject => "Here's Your Prediction!")
-    attachments.inline['fortune_600.png'] = File.read('fortune_600.png')
   end
 
   def self.send_todays_emails
